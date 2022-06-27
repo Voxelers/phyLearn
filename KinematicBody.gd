2a82ea29d17d839694310b9deaa4fbe63253be1f
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2022-06-27 13:32:19 +0200

    Try to sync rigid and kinematic fall

--- KinematicBody.gd
+++ KinematicBody.gd
@@ -6,21 +6,26 @@ var velocity = Vector3.DOWN
 
 var frames = 0
 
+signal start_rigid
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity = velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _physics_process(delta):
+	print("KinematicBody")
 	var speed = OS.get_ticks_msec()/1000 * (9.8/100)
 	velocity = Vector3.DOWN * speed
 	frames = frames + 1  # by default 60 fps
-	# velocity = velocity * 0.97
-	move_and_collide(velocity * speed)
+	move_and_collide(velocity)
+
+	# emit_signal("start_rigid")
 
 func _exit_tree():  # close the window
 	print("Total frames: ", frames)
+
+
+func _on_RigidBody_sleeping_state_changed():
+	print("RigidBody started") # Replace with function body.
