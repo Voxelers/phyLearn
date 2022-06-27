5179854356f163e3af6b57015edf8143600745d6
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2022-06-27 08:44:39 +0200

    Simple approach with constant velocity

--- KinematicBody.gd
+++ KinematicBody.gd
@@ -1,17 +1,15 @@
 extends KinematicBody
 
-export var speed = 0.14
+export var speed = 0.10
 
 var velocity = Vector3.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = Vector3.FORWARD * speed
-	velocity = velocity.rotated(Vector3.RIGHT, int(-PI/2))
+	velocity = Vector3.DOWN * speed
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move_and_collide(velocity)
-	# move_and_slide(velocity)
