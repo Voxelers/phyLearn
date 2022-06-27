5bd87e16f65c7a4c353b12ab78b1a0bae8bf0f0e
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2022-06-27 10:29:12 +0200

    Adding gravity force speed

--- KinematicBody.gd
+++ KinematicBody.gd
@@ -1,15 +1,26 @@
 extends KinematicBody
 
-export var speed = 0.10
+# export var speed = 0.10
 
-var velocity = Vector3.ZERO
+var velocity = Vector3.DOWN
+
+var frames = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity = Vector3.DOWN * speed
-
+	velocity = velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_and_collide(velocity)
+	pass
+
+func _physics_process(delta):
+	var speed = OS.get_ticks_msec()/1000 * (9.8/100)
+	velocity = Vector3.DOWN * speed
+	frames = frames + 1  # by default 60 fps
+	# velocity = velocity * 0.97
+	move_and_collide(velocity * speed)
+
+func _exit_tree():  # close the window
+	print("Total frames: ", frames)
